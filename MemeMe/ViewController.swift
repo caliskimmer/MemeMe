daa06229d3ce0e2a87ffc2016f45b47bccab04bf
caliskimmer <caliskimmer@gmail.com>
2018-07-17 14:18:09 -0700

    breaking ground

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Matas Empakeris on 7/13/18.
+//  Copyright © 2018 MatasEmpakeris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
